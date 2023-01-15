080bb814b34b7e579dca5a5103942a699b16cc19
Pavithra J <83458034+pavithra026@users.noreply.github.com>
2023-01-15 21:14:25 +0530

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,14 @@
+function matchpass(){  
+var username = document.f1.username.value;  
+var password=document.f1.password.value;  
+  
+if(username=="pavithra026" && password == "user123"){  
+return true;  
+}  
+else{  
+alert("Username or password is incorrect !");  
+return false;  
+}  
+}  
+  
+  
